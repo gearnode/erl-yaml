cf8b60581423cb69f86c714d73f42474251b57d3
Nicolas Martyanoff <khaelin@gmail.com>
2021-04-06 08:17:43 +0200

    initial import

---
+++ src/yaml_nif.erl
@@ -0,0 +1,28 @@
+%% Copyright (c) 2021 Nicolas Martyanoff <khaelin@gmail.com>.
+%%
+%% Permission to use, copy, modify, and/or distribute this software for any
+%% purpose with or without fee is hereby granted, provided that the above
+%% copyright notice and this permission notice appear in all copies.
+%%
+%% THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+%% WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+%% MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+%% SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+%% WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+%% ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+%% IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+-module(yaml_nif).
+
+-export([get_version_string/0]).
+
+-on_load(init/0).
+
+init() ->
+  PrivDir = code:priv_dir(yaml),
+  LibPath = filename:join([PrivDir, "yaml_nif"]),
+  erlang:load_nif(LibPath, []).
+
+-spec get_version_string() -> binary().
+get_version_string() ->
+  erlang:nif_error(nif_not_loaded).
